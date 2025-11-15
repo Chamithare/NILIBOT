bf21b58d9b446aaf965b71b66d266f4ac77fa39b
chamith araliya <166959556+Chamithare@users.noreply.github.com>
2025-11-15 20:45:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
 MONGO_URI = os.getenv("MONGODB_URI") or ""
 GROUP_ID = int(os.getenv("GROUP_ID") or 0)
-ADMIN_ID = int(os.getenv("ADMIN_ID") or 0)
+ADMIN_IDS = {int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x}
 DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID") or 0)
+
 
 
