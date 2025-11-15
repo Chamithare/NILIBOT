0cfe64cb4fa6e5e59611d32902e16ee6ba181ce9
chamith araliya <166959556+Chamithare@users.noreply.github.com>
2025-11-15 21:05:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
-# config.py
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GROUP_ID = os.getenv("GROUP_ID")
+DB_CHANNEL_ID = os.getenv("DB_CHANNEL_ID")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
-MONGO_URI = os.getenv("MONGODB_URI") or ""
-GROUP_ID = int(os.getenv("GROUP_ID") or 0)
-ADMIN_IDS = {int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x}
-DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID") or 0)
+# Multiple admins support
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
+
+
 
 
 
